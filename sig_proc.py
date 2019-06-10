6cb82cba461dd30301990a3151233b08d1227b7c
unknown <guy.pelc@gmail.com>
2019-06-10 22:42:43 +0300

    working example

--- sig_proc.py
+++ sig_proc.py
@@ -1,23 +1,35 @@
 from numpy import *
 from random import randint
+from numpy.fft import *
+
 
 def pic_to_arr(pic):
-    #CONVERT TO 1D ARRAY
+    # generate prefix
+    nrows,ncols = pic.shape
 
+    # generate prefix in format BLACK,ncols*WHITE,BLACK,nrows*WHITE,BLACK
+
+    prefix = array([])
+
+    prefix = append(prefix,255)
+    prefix = append(prefix,ones(ncols))
+    prefix = append(prefix,255)
+    prefix = append(prefix,ones(nrows))
+    prefix = append(prefix,255)
+    
+    #generate pic_arr
     row_pixels = array([])
-
     for row in pic:
         row_pixels = append(row_pixels,row)
-        row_pixels = append(row_pixels,[2]) #new row signal
-        print(row_pixels)
 
-    pic = row_pixels
-    #pad with end_pixel and new_row_pixel:
-    pic = append([1,2],pic)
-    pic= append(pic,[1])
-    pic = array([pic])
     
-    return pic
+    #concat and return
+    row_pixels = append(prefix,row_pixels)
+    row_pixels = reshape(row_pixels,(1,size(row_pixels)))
+    return row_pixels
+
+
+    
 
 def  modulate_arr(pic_arr,FREQ,TIME_PER_PIXEL,SAMPLE_RATE):
     t = arange(0,TIME_PER_PIXEL,1./SAMPLE_RATE)
@@ -37,79 +49,82 @@ def get_time_axis(signal,SAMPLE_RATE):
     return arange(0,size(signal)/SAMPLE_RATE,1/SAMPLE_RATE)
 
 
-"""
-assume knowledge of :
-    SAMPLE_RATE
-    FREQ
-    T_END time of broadcast in seconds per pixel
-"""
-def demodulate_signal(recieved_signal,SAMPLE_RATE,T_END):
+
+
+
+def process_psig(signal,SAMPLE_RATE,TPP):
+    cur_psig, r_signal = split(signal,[int(SAMPLE_RATE*TPP)])
+    fourier = rfft(cur_psig)/SAMPLE_RATE*2
+    cur_pamp = amax(abs(fourier))
+#     print("amp",cur_pamp)
     
-    time_axis=get_time_axis(recieved_signal,SAMPLE_RATE)
+    return r_signal,cur_pamp
+
+
+def get_dimensions(signal,SAMPLE_RATE,TPP):
+
+    r_signal,counter_amp = process_psig(signal,SAMPLE_RATE,TPP)
+
+    ncols,nrows = (0,0)
     
-        #first pixel is end amp:
-    cur_signal,recieved_signal = split(recieved_signal,[SAMPLE_RATE*T_END])
-    cur_time_axis,time_axis=split(time_axis,[SAMPLE_RATE*T_END])
-
-    fourier = fft.rfft(cur_signal)/SAMPLE_RATE*2
-    end_pixel = amax(abs(fourier))
-    print("end_pixel",end_pixel)
-
-    #second pixel is new row pixel
-    cur_signal,recieved_signal = split(recieved_signal,[SAMPLE_RATE*T_END])
-    cur_time_axis,time_axis=split(time_axis,[SAMPLE_RATE*T_END])
-
-    fourier = fft.rfft(cur_signal)/SAMPLE_RATE*2
-    # freq = fft.rfftfreq()
-    # fourier = 1*shift(fourier.real,-CARRIER_FREQ*T_END,cval=0)+1j*shift(fourier.imag,-CARRIER_FREQ*T_END,cval=0)
-    new_row_pixel = amax(abs(fourier))
-    # new_row_pixel = 2
-    print("new_row_pixel",new_row_pixel)
-
-    restored_2d = empty([0,0])
-    restored_pixels = empty([0,0])
-
-
-    # n_pixels = int(abs(size(recieved_signal)/SAMPLE_RATE))
-
-
+    #count number of cols
     while(True):
-        cur_signal,recieved_signal = split(recieved_signal,[SAMPLE_RATE*T_END])
-        cur_time_axis,time_axis=split(time_axis,[SAMPLE_RATE*T_END])
+        r_signal,cur_pamp = process_psig(r_signal,SAMPLE_RATE,TPP)
+        if (cur_pamp>1./2*counter_amp): 
+#             print("here",cur_pamp,1./2*counter_amp)
+            break
+        ncols += 1
+    
+#     print(ncols)
+    
+    while(True):
+        r_signal,cur_pamp = process_psig(r_signal,SAMPLE_RATE,TPP)
+        if (cur_pamp>1./2*counter_amp): 
+#             print(cur_pamp,2*counter_amp)
+            break
+        nrows += 1
 
-        fourier = fft.rfft(cur_signal)/SAMPLE_RATE*2
-    #     freq = fft.rfftfreq(cur_time_axis.shape[-1],1./SAMPLE_RATE)
+#     print(nrows)
+    print("DIM=",nrows,ncols)
+    
+    return ncols,nrows,r_signal
 
-        cur_pixel = amax(abs(fourier))
-
-        if cur_pixel<1.5*end_pixel:
-             print("reached end pixel")
-             print(cur_pixel)             
-
-             break
-
-        elif cur_pixel<1.5*new_row_pixel:
-            print("new row pixel")
-            if restored_2d.size==0:
-                restored_2d=restored_pixels
-            else:
-                restored_2d=append(restored_2d,restored_pixels,axis=0)
-
-            print("restored_pixel shape",restored_pixels.shape)
-            print("2d_pixel shape",restored_2d.shape)
-            restored_pixels=empty([0,0])
-            continue
+    
+#     ncols,nrows,r_signal = get_dimensions(signal,SAMPLE_RATE,TPP)
 
 
+def process_row(r_signal,SAMPLE_RATE,TPP,ncols):
+    
+    row = array([])
+    for i in range(ncols):
+        r_signal,cur_pamp = process_psig(r_signal,SAMPLE_RATE,TPP)
 
-        restored_pixels = append(restored_pixels, amax(abs(fourier))).reshape(1,restored_pixels.size+1)
-        print(restored_pixels)             
+        row = append(row,cur_pamp)
+#         print(row)
+    row = reshape(row,(1,size(row)))
+#     print('row shape:',row.shape)
+    return row,r_signal 
+    
+# row,r_signal=process_row(r_signal,SAMPLE_RATE,TPP,ncols)
 
-    # for i in range(n_pixels):
 
-    print("restored 2d is:",restored_2d)
-    return restored_2d
+def process_signal(signal,SAMPLE_RATE,TPP):
+    
+    ncols,nrows,r_signal = get_dimensions(signal,SAMPLE_RATE,TPP)
+    
+    restored_pic,r_signal = process_row(r_signal,SAMPLE_RATE,TPP,ncols)
+#     restored_pic = restored_pic.reshape(1,ncols)
+#     print('shape',restored_pic.shape)
 
+    for r in range(nrows-1):
+        row,r_signal=process_row(r_signal,SAMPLE_RATE,TPP,ncols)
+        
+        
+        restored_pic = append(restored_pic,row,axis=0)
+        
+#         print('shape',restored_pic.shape)
+    return restored_pic
 
-
+    # signal = loadtxt(open(SIGNAL_FILE))
+    # process_signal(signal,SAMPLE_RATE,TPP)
     
